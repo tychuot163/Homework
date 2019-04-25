856f80a8d0a360f01b627e9bc745d001781cd628
Nguyen Anh Tu <tu.lqd.163@gmail.com>
2019-04-25 15:14:10 +0700

    Homework

---
+++ Tusturu/MVC/MVC.Tests/Controllers/Test.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC.Controllers;
+using MVC.Models;
+using MVC;
+using System.Text;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+namespace MVC.Tests.Controllers
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new CS4PEEntities();
+            var controller = new VLTeaController();
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            var model = result.Model as List<BubleTea>;
+            Assert.IsInstanceOfType(result.Model, typeof(List<BubleTea>));
+            Assert.AreEqual(db.BubleTeas.Count(), (model.Count));
+        }
+        [TestMethod]
+        public void TestCreate()
+        {
+            var controller = new VLTeaController();
+            var result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestEditG()
+        {
+            var controller = new VLTeaController();
+            var db = new CS4PEEntities();
+            var result0 = controller.Edit(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+            var item = db.BubleTeas.First();
+            var result1 = controller.Edit(item.id) as ViewResult;
+            Assert.IsNotNull(result1);
+            var model = result1.Model as BubleTea;
+            Assert.AreEqual(item.id, model.id);
+        }
+        [TestMethod]
+        public void TestDetails()
+        {
+            var controller = new VLTeaController();
+            var db = new CS4PEEntities();
+            var item = db.BubleTeas.First();
+
+            var result = controller.Details(item.id);
+            var view = result as ViewResult;
+            Assert.IsNotNull(view);
+            var model = view.Model as BubleTea;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(item.id, model.id);
+
+            var result0 = controller.Details(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+        }
+         [TestMethod]
+        public void TestDelete()
+        {
+            var controller = new VLTeaController();
+            var db = new CS4PEEntities();
+            var item = db.BubleTeas.First();
+
+            var result = controller.Delete(item.id);
+            var view = result as ViewResult;
+            Assert.IsNotNull(view);
+            var model = view.Model as BubleTea;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(item.id, model.id);
+
+            var result0 = controller.Delete(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+        }
+         [TestMethod]
+         public void TestCreated()
+         {  
+            var db = new CS4PEEntities();
+            var model = new BubleTea
+            {
+                Name = "Tra sua VL",
+                Price = 25000,
+                Topping = "Tran chau trang"
+            };
+            var controller = new VLTeaController();
+            var result = controller.Create(model);
+            var redirect = result as RedirectToRouteResult;
+            Assert.IsNotNull(redirect);
+            Assert.AreEqual("Index", redirect.RouteValues["action"]);
+            var item = db.BubleTeas.Find(model.id);
+            Assert.IsNotNull(item);
+            Assert.AreEqual(model.Name, item.Name);
+            Assert.AreEqual(model.Price, item.Price);
+            Assert.AreEqual(model.Topping, item.Topping);
+         }
+    }
+    
+}
---
+++ Tusturu/MVC/MVC/Controllers/VLTeaController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class VLTeaController : Controller
+    {
+        private CS4PEEntities db = new CS4PEEntities();
+
+        // GET: /VLTea/
+        public ActionResult Index()
+        {
+            return View(db.BubleTeas.ToList());
+        }
+
+        // GET: /VLTea/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            if (bubletea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bubletea);
+        }
+
+        // GET: /VLTea/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /VLTea/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create( BubleTea bubletea)
+        {
+            KiemtraGia(bubletea);
+            if (ModelState.IsValid)
+            {
+                db.BubleTeas.Add(bubletea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bubletea);
+        }
+        public void KiemtraGia(BubleTea model)
+        {
+            if (model.Price <= 0)
+            {
+                ModelState.AddModelError("price", "Số tiền quá ít");
+            }
+        }
+        // GET: /VLTea/Edit/5
+        public ActionResult Edit(int id)
+        {
+       
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            if (bubletea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bubletea);
+        }
+
+        // POST: /VLTea/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,Name,Price,Topping")] BubleTea bubletea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bubletea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bubletea);
+        }
+
+        // GET: /VLTea/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            if (bubletea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bubletea);
+        }
+
+        // POST: /VLTea/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BubleTea bubletea = db.BubleTeas.Find(id);
+            db.BubleTeas.Remove(bubletea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
